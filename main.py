dfd698920f2a90d90459eb3741a8e2827a987549
Tei06 <86541709+Tei06@users.noreply.github.com>
2021-08-02 22:56:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 from dotenv import load_dotenv
 from discord.ext import commands
 import json
-from pathlib import Path
 import random
 load_dotenv()
 
@@ -17,10 +16,59 @@ prefixes = ["?","shiz ","Shiz", 's!']
 bot = commands.Bot(command_prefix=prefixes, intents=discord.Intents.all())
 
 
+@bot.event
+async def on_message(message):
+    empty_array = []
+    modmail_channel = discord.utils.get(bot.get_all_channels(), name="hand-pics")
+
+    if message.author == bot.user:
+        return
+    if str(message.channel.type) == "private":
+        if message.attachments != empty_array:
+            files = message.attachments
+            await modmail_channel.send("[" + message.author.display_name + f":{message.author.id}" + "]")
+
+            for file in files:
+                await modmail_channel.send(file.url)
+        else:
+            await modmail_channel.send("[" + message.author.display_name + "] " + message.content)
+
+    elif str(message.channel) == "hand-pics" and message.content.startswith("<"):
+        member_object = message.mentions[0]
+        if message.attachments != empty_array:
+            files = message.attachments
+            await member_object.send("[" + message.author.display_name + "]")
+
+            for file in files:
+                await member_object.send(file.url)
+        else:
+            index = message.content.index(" ")
+            string = message.content
+            mod_message = string[index:]
+            await member_object.send("[" + message.author.display_name + "]" + mod_message)
+
+    await bot.process_commands(message)
+
 
 @bot.event
-async def when_mentioned():
-    await bot.send("My prefixes are `s!`, `?`, `shizuku`")
+async def on_ready():
+    await bot.change_presence(activity=discord.Streaming(name='s!help | ur questionable', url='https://www.twitch.tv/chillhopmusic'))
+    print('Connected to bot: {}'.format(bot.user.name))
+    print('Bot ID: {}'.format(bot.user.id))
+    channel = bot.get_channel(845354439204012052)
+    await channel.send("Bot is now online")
+    bruh = os.getcwd()
+    print(bruh)
+
+@bot.event
+async def on_guild_join(guild):
+    with open('prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+
+    prefixes[str(guild.id)] = '?'
+    
+    with open('prefixes.json', 'w') as f:
+        json.dump(prefixes, f, indent=4)
 
 
 @bot.command()
